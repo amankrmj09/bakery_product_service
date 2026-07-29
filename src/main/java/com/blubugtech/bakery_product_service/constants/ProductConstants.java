162fcbbde5a86ed403bc0847ce26d303326040f2
Aman Kumar Maurya <amankrmj@gmail.com>
2026-07-29 21:09:02 +0530

    Refactor: Centralize domain constants and update core dependencies

---
+++ src/main/java/com/blubugtech/bakery_product_service/constants/ProductConstants.java
@@ -0,0 +1,10 @@
+package com.blubugtech.bakery_product_service.constants;
+
+public final class ProductConstants {
+    private ProductConstants() {}
+
+    public static final String PRODUCTS_CACHE = "products";
+    public static final String CATEGORIES_CACHE = "categories";
+    public static final String STORE_SETTINGS_CACHE = "store_settings";
+    public static final String DEFAULT_ID = "default";
+}
