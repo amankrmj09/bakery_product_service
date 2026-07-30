f2f484103120db541f2dd68cfaef2bf9ae35bcfc
Aman Kumar Maurya <amankrmj@gmail.com>
2026-07-30 10:34:41 +0530

    chore: bump bakery_common_security version to 1.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ repositories {
 extra["springCloudVersion"] = "2025.0.3"
 
 dependencies {
-    implementation("org.blubakery.libs:bakery_common_security:1.0.2")
+    implementation("org.blubakery.libs:bakery_common_security:1.0.3")
     implementation("org.blubakery.libs:bakery_common_messaging:1.0.1")
     implementation("org.blubakery.libs:bakery_common_core:1.0.2")
     implementation("org.blubakery.libs:bakery_common_feign:1.0.3")
